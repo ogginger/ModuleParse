3a6e199e277284cdb2fabc733afadc129326152b
Josh <og.ginger@gmail.com>
2020-01-15 05:37:29 +0000

    Updated parseValidation: Changed 'Error: The define statement has no content.' validation to allow for multiline wildcards.

--- parseValidation.js
+++ parseValidation.js
@@ -3,9 +3,9 @@ define([], function() {
         function( Input ) {
             if ( typeof Input.Options != "string" ) {
                 throw {  message: "Error: The input type was not string." };
-            } else if ( Input.Options.match(/^.*define\(.*\);\s*$/mgi) == null ) {
+            } else if ( Input.Options.match(/^[\s\S]*define\([\s\S]*\);\s*$/i) == null ) {
                 throw { message: "Error: Module does not contain a valid define statement." }
-            } else if (Input.Options.match(/^.*define\(.*({.*}|function\s*\(.*\)\s*{.*})\s*\);\s*$/mgi) == null ) {
+            } else if (Input.Options.match(/^[\s\S]*define\(\s*({[\s\S]*}|function\s*\([\s\S]*\)\s*{[\s\S]*})\s*\);\s*$/gi) == null ) {
                 throw { message: "Error: The define statement has no content." }
             } else {
                 return true;
